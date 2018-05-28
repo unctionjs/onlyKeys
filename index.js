ff6d25fd10285ea5ca8f306873ef28fd3532c0ef
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-28 13:42:49 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,35 +1,10 @@
-import mergeRight from "@unction/mergeright"
-import reduceValues from "@unction/reducevalues"
-import objectFrom from "@unction/objectfrom"
-import get from "@unction/get"
-import fresh from "@unction/fresh"
-
-import type {ArrayType} from "types"
-import type {KeyType} from "types"
-import type {UnaryFunctionType} from "types"
-import type {KeyedEnumerableType} from "types"
-
-export default function onlyKeys (keys: ArrayType<KeyType>): UnaryFunctionType {
-  return function onlyKeysKeys (keyedEnumerable: KeyedEnumerableType): KeyedEnumerableType {
-    return reduceValues(
-      (accumulated: KeyedEnumerableType): UnaryFunctionType =>
-        (key: KeyType): KeyedEnumerableType => mergeRight(
-          accumulated
-        )(
-          objectFrom(
-            [key]
-          )(
-            get(
-              key
-            )(
-              keyedEnumerable
-            )
-          )
-        )
-    )(
-      fresh(keyedEnumerable)
-    )(
-      keys
-    )
-  }
+import mergeRight from "@unction/mergeright";
+import reduceValues from "@unction/reducevalues";
+import objectFrom from "@unction/objectfrom";
+import get from "@unction/get";
+import fresh from "@unction/fresh";
+export default function onlyKeys(keys) {
+  return function onlyKeysKeys(keyedEnumerable) {
+    return reduceValues(accumulated => key => mergeRight(accumulated)(objectFrom([key])(get(key)(keyedEnumerable))))(fresh(keyedEnumerable))(keys);
+  };
 }
