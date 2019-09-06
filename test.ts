75d1a9c75af67219fc009f3c8c2392f6bfdc48cc
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-06 13:08:10 -0700

    Continuing to setup testing

--- test.ts
+++ test.ts
@@ -1,6 +1,6 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type, no-undefined */
 
-import onlyKeys from "./index.ts"
+import onlyKeys from "./index"
 
 test("onlyKeys with present keys", () => {
   expect(onlyKeys(["alpha", "beta", "delta"])({
